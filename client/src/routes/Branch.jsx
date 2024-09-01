c6b176ff28c3a55611eb55875031a9250b571c35
aniket <aniket@gamil.com>
2024-09-01 16:27:47 +0530

    fixed update branch for deployment

--- client/src/routes/Branch.jsx
+++ client/src/routes/Branch.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import AddNewBranch from '../pages/AddNewBranch';
 import ListBranch from '../pages/ListBranch';
-import UpdateBranch from '../pages/updateBranch';
+import UpdateBranch from '../pages/UpdateBranch';
 
 const Branch = () => {
   return (
